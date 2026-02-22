c31b0a3bf35bd87c5c1618ee03d53b12a2bec6b4
ygteker <gunes.teker@gmail.com>
2026-02-22 21:40:03 +0100

    Initial Commit

---
+++ horizon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  horizon
+//
+//  Created by Yagiz Gunes Teker on 22.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ horizon/horizonApp.swift
@@ -0,0 +1,17 @@
+//
+//  horizonApp.swift
+//  horizon
+//
+//  Created by Yagiz Gunes Teker on 22.02.26.
+//
+
+import SwiftUI
+
+@main
+struct horizonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
